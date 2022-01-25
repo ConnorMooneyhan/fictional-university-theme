9b5dfee8ea43fc3b29c5afa50b8d80ee7dfd4d77
Connor Mooneyhan <connmoo@gmail.com>
2022-01-25 18:13:48 -0500

    Figured I'd make this a git repo to track my progress on GitHub

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'paulthewalton/acf-stubs' => 
+    array (
+      'pretty_version' => '5.8.7',
+      'version' => '5.8.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '722096c127186c41ac98395b418195d1db9cf576',
+    ),
+  ),
+);
